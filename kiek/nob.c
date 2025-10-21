d66b6bb637723bb4ee00d0ad69e545fc8ac5335d
espinosa05 <francisco-fischer@gmx.de>
2025-10-21 09:04:43 +0200

    fixed nob.c include path flag

--- kiek/nob.c
+++ kiek/nob.c
@@ -6,7 +6,7 @@
 #define OUT_DIR "out/"
 
 #define CC      "gcc"
-#define CFLAGS  "-I../../include", "-ggdb", "-Wall", "-std=gnu11", "-Wextra", "-pedantic", "-Werror", "-c"
+#define CFLAGS  "-I../include", "-ggdb", "-Wall", "-std=gnu11", "-Wextra", "-pedantic", "-Werror", "-c"
 #define LIBNAME "kiek"
 
 #define AR      "ar"
